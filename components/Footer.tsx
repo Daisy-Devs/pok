5973663275d399f6b7ce941fd74d1ca455736905
SuchitaGanati <suchitaganati2002@gmail.com>
2026-03-24 13:41:21 +0530

    added dropdown,footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-background-secondary text-gray-700 py-5 px-5">
       <div className=" flex flex-col">
         {/* Top Section */}
-        <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 gap-4 pb-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 pb-6">
           {/* Brand */}
           <div className="flex flex-col gap-3">
             <h1 className="font-semibold">Proof of Kindness</h1>
@@ -15,44 +15,81 @@ export default function Footer() {
             </p>
           </div>
 
-          {/* Ecosystem */}
+          {/* Get Started */}
           <div className="flex flex-col gap-2">
-            <h2 className="font-medium">Ecosystem</h2>
+            <h2 className="font-medium">Get Started</h2>
             <ul className="flex flex-col gap-1 text-sm text-gray-600">
-              <li className="hover:text-black cursor-pointer">WhitePaper</li>
               <li className="hover:text-black cursor-pointer">
-                Smart Contracts
-              </li>
-              <li className="hover:text-black cursor-pointer">
-                Network Status
+                Fundraising Solution
               </li>
               <li className="hover:text-black cursor-pointer">Campaign</li>
             </ul>
           </div>
 
-          {/* Legal */}
+          {/* Give Now */}
           <div className="flex flex-col gap-2">
-            <h2 className="font-medium">Legal</h2>
+            <h2 className="font-medium">Give Now</h2>
             <ul className="flex flex-col gap-1 text-sm text-gray-600">
-              <li className="hover:text-black cursor-pointer">Support</li>
               <li className="hover:text-black cursor-pointer">
-                Privacy Policy
+                Make a Donation
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Anomous Donor Service
               </li>
             </ul>
           </div>
 
+          {/* Resources */}
           <div className="flex flex-col gap-2">
-            <h2 className="font-medium">Legal</h2>
+            <h2 className="font-medium">Resources</h2>
             <ul className="flex flex-col gap-1 text-sm text-gray-600">
-              <li className="hover:text-black cursor-pointer">Support</li>
               <li className="hover:text-black cursor-pointer">
-                Privacy Policy
+                Resources for Nonprofits
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Resources for Donors
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Crypto Staking
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Developer Resources
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Cryptocurrency
+              </li>
+              <li className="hover:text-black cursor-pointer">FAQ</li>
+            </ul>
+          </div>
+          {/* Partnership */}
+          <div className="flex flex-col gap-2">
+            <h2 className="font-medium">Integration Partners</h2>
+            <ul className="flex flex-col gap-1 text-sm text-gray-600">
+              <li className="hover:text-black cursor-pointer">
+                Crypto Industry Partners
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Media Partners
+              </li>
+              <li className="hover:text-black cursor-pointer">
+                Partner with Us
+              </li>
+            </ul>
+          </div>
+
+          {/* About */}
+          <div className="flex flex-col gap-2">
+            <h2 className="font-medium">About</h2>
+            <ul className="flex flex-col gap-1 text-sm text-gray-600">
+              <li className="hover:text-black cursor-pointer">Events</li>
+              <li className="hover:text-black cursor-pointer">Contact</li>
+              <li className="hover:text-black cursor-pointer">Security</li>
+              <li className="hover:text-black cursor-pointer">
+                Supported Countries
               </li>
             </ul>
           </div>
         </div>
-
-        
 
         {/* Bottom Section */}
         <div className="flex justify-center items-center border-t pt-4 text-sm text-gray-500 text-center">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,24 @@
-"use client"
+"use client";
 
-import { nomenclature } from '@/src/constants/nomenclature'
-import { Bell, UserCircle2 } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from './ui/button'
-import { usePathname } from 'next/navigation'
+import { nomenclature } from "@/src/constants/nomenclature";
+import { Bell, UserCircle2 } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
-    const isLoggedIn = true
-     const pathname = usePathname();
+  const isLoggedIn = true;
+  const pathname = usePathname();
   return (
     <div className="h-16 px-6 flex items-center justify-between">
-
       {/* Logo */}
       <Link href="/">
         <p className="text-xl font-extrabold text-tertiary">
@@ -22,34 +28,56 @@ const Navbar = () => {
 
       {/* Navigation */}
       <div className="flex items-center gap-6">
-        <Link href="/" className={`text-sm font-normal hover:underline underline-offset-4 ${pathname === '/' && 'underline decoration-primary decoration-wavy decoration-3'}`}>
+        <Link
+          href="/"
+          className={`text-sm font-normal hover:underline underline-offset-4 ${pathname === "/" && "underline decoration-primary decoration-wavy decoration-3"}`}
+        >
           {nomenclature.HOME}
         </Link>
-        <Link href="/explore" className={`text-sm font-normal hover:underline underline-offset-8 ${pathname === '/explore' && 'underline decoration-primary decoration-wavy decoration-3'}`}>
+        <Link
+          href="/explore"
+          className={`text-sm font-normal hover:underline underline-offset-8 ${pathname === "/explore" && "underline decoration-primary decoration-wavy decoration-3"}`}
+        >
           {nomenclature.EXPLORE_CAMPAIGNS}
         </Link>
-        <Link href="/about" className={`text-sm hover:underline underline-offset-8 ${pathname === '/about' && 'underline decoration-primary decoration-wavy decoration-3'}`}>
+        <Link
+          href="/about"
+          className={`text-sm hover:underline underline-offset-8 ${pathname === "/about" && "underline decoration-primary decoration-wavy decoration-3"}`}
+        >
           {nomenclature.ABOUT_US}
         </Link>
       </div>
-  {  isLoggedIn ?
-    /**  Icons **/
-      <div className="flex items-center gap-4">
-        <Bell size={20} className="cursor-pointer" />
-        <UserCircle2 size={20} className="cursor-pointer" />
-      </div>
-      :
-      <div className="flex items-center gap-4">
-        <Link href="/" className="text-sm hover:underline">
-          <Button text={nomenclature.NGO_PORTAL}/>
-        </Link>
-        <Link href="/" className="text-sm hover:underline">
-          <Button text={nomenclature.SIGN_IN}/>
-        </Link>
-      </div>
-}
-    </div>
-  )
-}
+      {isLoggedIn ? (
+        /**  Icons **/
+        <div className="flex items-center gap-4">
+          <Bell size={20} className="cursor-pointer" />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <UserCircle2 size={20} className="cursor-pointer" />
+            </DropdownMenuTrigger>
 
-export default Navbar
+            <DropdownMenuContent align="end" className="w-48">
+              <DropdownMenuItem asChild>
+                <Link href="/">Connect Wallet</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-500">
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <Link href="/" className="text-sm hover:underline">
+            <Button text={nomenclature.NGO_PORTAL} />
+          </Link>
+          <Link href="/" className="text-sm hover:underline">
+            <Button text={nomenclature.SIGN_IN} />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
