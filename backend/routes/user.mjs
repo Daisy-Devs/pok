076f873efee1dd947dfb61807a8e434d62e3bc00
Pranita R <pranita@theunmarketing.agency>
2026-04-08 16:39:38 +0530

    Fix

--- backend/routes/user.mjs
+++ backend/routes/user.mjs
@@ -19,7 +19,7 @@ router.post('/login', loginUser);
 router.post('/google', googleAuth);
 router.post('/apple', appleAuth);
 router.post('/forgotPassword', forgotPassword);
-router.post('/resetPassword', resetPassword)
+router.post('/resetPassword/:token', resetPassword)
 router.post('/logout', logoutUser);
 router.post('/wallet/logout', logoutWallet);
 router.get('/me', getCurrentUser);
