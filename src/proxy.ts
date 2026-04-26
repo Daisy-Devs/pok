34893ee1047903f1f52ada2c4e08dd5f2070720f
Riya Vechiot <vechiotriya@gmail.com>
2026-04-26 21:26:56 +0530

    fix: user logged in as ngo when tries to navigate to /sign-in is taken ngo

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,58 +1,56 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import jwt from 'jsonwebtoken';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
 
 export const config = {
-  matcher: ['/ngo/:path*','/profile','/sign-in', '/register'], //TODO: add donate '/campaigns/:path*/donate'
-  
+  matcher: ["/ngo/:path*", "/profile", "/sign-in", "/register"], //TODO: add donate '/campaigns/:path*/donate'
 };
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
-const token = request.cookies.get('token')?.value || "";
-const role = token ? jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!) as string:"";
+  const token = request.cookies.get("token")?.value || "";
+  const destructuredToken = token
+    ? (jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!) as string)
+    : "";
+  const role = destructuredToken ? destructuredToken?.role : "";
 
   if (
-    pathname.startsWith('/ngo/register') ||
-    pathname.startsWith('/ngo/sign-in')
+    pathname.startsWith("/ngo/register") ||
+    pathname.startsWith("/ngo/sign-in")
   ) {
-    if (token && role == 'ngo') {
-      return NextResponse.redirect(new URL('/ngo', request.url));
+    if (token && role == "ngo") {
+      return NextResponse.redirect(new URL("/ngo", request.url));
     }
     return NextResponse.next();
   }
 
-  if (pathname.startsWith('/sign-in') || pathname.startsWith('/register')) {
-    if (token) {
-      if(role==='donor'){
-        return NextResponse.redirect(new URL('/', request.url));
-      }
-      return NextResponse.redirect(new URL('/ngo', request.url));
+  if (pathname.startsWith("/sign-in") || pathname.startsWith("/register")) {
+    if (token && role === "donor") {
+      return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
   }
 
-  if (pathname.startsWith('/profile')) {
-    if (!token && role==='donor') {
-      return NextResponse.redirect(new URL('/sign-in', request.url));
+  if (pathname.startsWith("/profile")) {
+    if (!token && role === "donor") {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
     }
-    if(token && role==='ngo'){
-      return NextResponse.redirect(new URL('/ngo', request.url));
+    if (token && role === "ngo") {
+      return NextResponse.redirect(new URL("/ngo", request.url));
     }
     return NextResponse.next();
   }
 
   // 🔒 Protect NGO routes
-  if (pathname.startsWith('/ngo')) {
-    if(role==='donor'){
-      return NextResponse.redirect(new URL('/', request.url));
+  if (pathname.startsWith("/ngo")) {
+    if (role === "donor") {
+      return NextResponse.redirect(new URL("/", request.url));
     }
     if (!token) {
-      return NextResponse.redirect(new URL('/ngo/sign-in', request.url));
+      return NextResponse.redirect(new URL("/ngo/sign-in", request.url));
     }
-
   }
   // 🔒 Protect payment/donation routes
-/*   if (pathname.includes('/donate')) {
+  /*   if (pathname.includes('/donate')) {
     if (!token) {
       return NextResponse.redirect(
         new URL(`/sign-in`, request.url)
