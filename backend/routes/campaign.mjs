9d8eb86bbe449dc6f19784fab97a2658ebcdd849
Pranita R <pranita@theunmarketing.agency>
2026-04-29 17:02:37 +0530

    Added toatal donors

--- backend/routes/campaign.mjs
+++ backend/routes/campaign.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCampaign, createOrgAndCampaign, getAllCampaigns, getCampaignsByOrganization, updateCampaign } from '../controllers/campaign.mjs';
+import { createCampaign, createOrgAndCampaign, getAllCampaigns, getCampaignById, getCampaignsByOrganization, updateCampaign } from '../controllers/campaign.mjs';
 import { ngoAuth } from '../middleware/ngoAuth.mjs';
 
 const router = express.Router();
@@ -9,7 +9,9 @@ router.post('/create', ngoAuth, createCampaign);
 router.post('/:id', ngoAuth, updateCampaign);
 
 router.get('/', getAllCampaigns);
+router.get('/:campaignId', getCampaignById);
 router.get('/:id', getCampaignsByOrganization);
+
 
 
 export default router;
