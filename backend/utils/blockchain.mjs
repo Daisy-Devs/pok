7c4c13dbeb7a09876d26e4f24f931f22a34cf15e
Pranita R <pranita@theunmarketing.agency>
2026-04-30 11:37:13 +0530

    updated provider

--- backend/utils/blockchain.mjs
+++ backend/utils/blockchain.mjs
@@ -1,6 +1,8 @@
 import { ethers } from "ethers";
 
-const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const provider = new ethers.WebSocketProvider(
+  `wss://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
+);
 
 // 🧪 TEST CONNECTION (temporary)
 provider.getBlockNumber().then(block => {
