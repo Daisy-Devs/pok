ddac81b72d0fe313dd89619ba15077de9317ad5a
SuchitaGanati <suchitaganati2002@gmail.com>
2026-05-13 19:37:29 +0530

    sidebar fixed

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -122,10 +122,15 @@ function SidebarProvider({
     <SidebarContext.Provider value={contextValue}>
       <div
         data-slot="sidebar-wrapper"
+        style={
+          {
+            "--sidebar-width": SIDEBAR_WIDTH,
+            "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
+            ...style,
+          } as React.CSSProperties
+        }
         className={cn(
           "group/sidebar-wrapper flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
-          `[--sidebar-width:${SIDEBAR_WIDTH}]`,
-          `[--sidebar-width-icon:${SIDEBAR_WIDTH_ICON}]`,
           className,
         )}
         {...props}
