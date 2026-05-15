a17951f54a98e04c5cb91ac51660e7a78b1bb76e
Riya Vechiot <vechiotriya@gmail.com>
2026-05-15 22:30:35 +0530

    feat: implement AlertDialog component for cookie consent notification

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,24 +1,47 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/src/components/ui/alert-dialog";
 import CampaignCauses from "@/src/features/home/components/CampaignCauses";
 import HeroSection from "@/src/features/home/components/HeroSection";
 import Philanthropy from "@/src/features/home/components/Philanthropy";
-import { ShieldAlert } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(() => {
+    const hasVisited = localStorage.getItem("hasVisited");
+    if (!hasVisited) {
+      localStorage.setItem("hasVisited", "true");
+      return true;
+    }
+    return false;
+  });
   return (
     <div className="bg-white overflow-x-hidden">
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent size="sm">
+          <AlertDialogHeader>
+            <AlertDialogTitle>⚠️ Third Party Cookies Required</AlertDialogTitle>
+            <AlertDialogDescription>
+              This app uses cookies for secure login. Please make sure to allow
+              third-party cookies for this website.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Okay</AlertDialogCancel>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
       <HeroSection />
-
       <Philanthropy />
-
       <CampaignCauses />
-      <div className="w-full flex justify-center my-7">
-      <div className="w-md gap-3 border-3 border-amber-300 bg-amber-50 flex flex-col justify-center items-center p-5 rounded-xl">
-        <div className="flex gap-2"><ShieldAlert className="text-amber-600" size={30}/> <span className="font-bold text-xl text-amber-600">This is not a real donation platform</span></div>
-        <p className="text-sm text-amber-400">This project runs on a blockchain testnet for demonstration and testing purposes only. No real cryptocurrency or funds are collected, processed, or disbursed. All tokens used here have no monetary value.</p>
-      </div>
-      </div>
     </div>
   );
 }
